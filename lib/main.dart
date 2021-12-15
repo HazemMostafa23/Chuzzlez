42a79812958e093c432296985749ed16fa24a3ce
marwan hashem <marawan.hashem@gmail.com>
2021-12-15 21:15:03 +0200

    test navigation

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'screens/home_screen.dart';
 import 'package:chuzzlez/providers/user_provider.dart';
+import 'package:chuzzlez/providers/board_provider.dart';
 import 'screens/board.dart';
 
 void main() {
@@ -17,6 +18,9 @@ class MyApp extends StatelessWidget {
       providers: [
         ChangeNotifierProvider(
           create: (context) => UserProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => BoardProvider(),
         )
       ],
       child: MaterialApp(
