033b9fe15505ff0419775ae5040d70458fbd24d2
Ahmed <memorykart1@gmail.com>
2022-01-18 02:22:16 +0200

    fixes

--- lib/main.dart
+++ lib/main.dart
@@ -52,7 +52,7 @@ class MyApp extends StatelessWidget {
         // )
       ],
       child: MaterialApp(
-        initialRoute: '/home',
+        initialRoute: '/login',
         routes: {
           '/home': (context) => HomeScreen(),
           '/puzzle': (context) => PuzzleBoardScreen(),
